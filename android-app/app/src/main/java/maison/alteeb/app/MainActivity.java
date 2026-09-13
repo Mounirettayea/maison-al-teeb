bf0de0960dc92c367369e2d90b31ff8feb630e08
Mounirettayea <76437728+Mounirettayea@users.noreply.github.com>
2026-09-13 12:52:18 -0700

    Add Maison Al Teeb WebView activity

---
+++ android-app/app/src/main/java/maison/alteeb/app/MainActivity.java
@@ -0,0 +1,60 @@
+package maison.alteeb.app;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.graphics.Color;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        webView.setBackgroundColor(Color.rgb(250, 248, 245));
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setLoadWithOverviewMode(false);
+        settings.setUseWideViewPort(false);
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String host = request.getUrl().getHost();
+                if (host != null && (host.equals("maisonalteeb.ma") || host.endsWith(".maisonalteeb.ma") || host.endsWith("vercel.app"))) {
+                    return false;
+                }
+                return false;
+            }
+        });
+
+        webView.loadUrl("https://www.maisonalteeb.ma/");
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
